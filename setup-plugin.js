dce8dd3b6cd047e67cde31fa6359a9d4caa14cc2
aubford <musicaubrey@gmail.com>
2018-12-13 13:33:42 -0700

    complete plugin

--- setup-plugin.js
+++ setup-plugin.js
@@ -5,31 +5,24 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import ReactDOM         from 'react-dom'
 import * as React       from 'react'
 import {formatHtml}     from './util'
+import { Provider } from 'react-redux'
 
 export const before = config => {
   console.logg = console.log
 
-  const muiTheme = getMuiTheme({
-    palette: {
-      primary1Color: 'rgb(0,167,225)', disabledColor: 'rgb(118,118,121)'
-    }, menuItem: {
-      selectedTextColor: 'rgb(0,167,225)'
-    }, datePicker: {
-      selectColor: 'rgb(0,167,225)'
-    }, timePicker: {
-      headerColor: 'rgb(0,167,225)', textColor: 'rgb(255,255,255)'
-    }, appBar: {
-      color: 'rgb(255,255,255)'
-    }
-  })
+  const muiTheme = getMuiTheme()
 
-  global.renderQuokka = comp => {
-    const Root = () => (<MuiThemeProvider muiTheme={muiTheme}>
+  global.quokka = (comp, Store) => {
+    let Root = () => (
+      <MuiThemeProvider muiTheme={muiTheme}>
         <div id={'comp-container'}>
           {comp}
         </div>
       </MuiThemeProvider>
     )
+    if (Store) {
+      Root = <Provider store={Store}>{Root}</Provider>
+    }
     ReactDOM.render(<Root/>, document.getElementById('root'))
     const html = document.getElementById('comp-container').innerHTML
     console.logg(formatHtml(html))
