4a924d83e10644c6a3a8f9ae2866bfd48a840f4a
aubford <musicaubrey@gmail.com>
2019-04-13 15:37:38 -0600

    remove unnecessary shit from other branches

--- setup-plugin.js
+++ setup-plugin.js
@@ -1,27 +1,3 @@
-import '@babel/polyfill'
-import 'raf/polyfill'
-import * as React       from 'react'
-import ReactDOM         from 'react-dom'
-import {formatHtml}     from './util'
-import { Provider } from 'react-redux'
-
 export const before = config => {
   console.logg = console.log
-
-  global.quokka = (comp, Store) => {
-    class Root extends React.Component {
-      render() {
-        return (
-        <div id={'comp-container'}>
-          {comp}
-        </div>
-    )}
-    }
-    if (Store) {
-      Root = <Provider store={Store}>{<Root />}</Provider>
-    }
-    ReactDOM.render(<Root/>, document.getElementById('root'))
-    const html = document.getElementById('comp-container').innerHTML
-    console.log(formatHtml(html))
-  }
 }
